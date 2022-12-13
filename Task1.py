aaf2fe32042d6ca3aa072a25fc2cc7423ec0afaf
yanochkina_sl <yanochkinasvetlana@mail.ru>
2022-12-13 18:32:56 +0600

    Add task2 at the same file

--- Task1.py
+++ Task1.py
@@ -1,4 +1,4 @@
-
+#Задание 1
 class Item:
     def __init__(self, name, price, quantity = 0):
         if self.__check(name, price, quantity):
@@ -19,7 +19,22 @@ class Item:
         else:
             return True
 
-print(Item('phone', 18000, 5))
+#print(Item('phone', 18000, 5))
 # print(Item(18000, 'phone', 5))
 # print(Item('phone', '18000', 5))
 # print(Item('phone', 18000, 5.5))
+
+#Задание 2
+
+class Phone(Item):
+    def __init__(self,name, price, quantity = 0, broken_phone = 0):
+        super().__init__(name, price, quantity)
+        self.broken_phone = broken_phone
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.price}, {self.quantity}, {self.broken_phone})"
+
+
+
+phone1 = Phone("iPhone 10", 500, 5, 1)
+print(phone1)
